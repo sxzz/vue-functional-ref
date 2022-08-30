ae38a057ebceeca0dd2afdd8caf28414aeece73a
三咲智子 <sxzz@sxzz.moe>
2022-08-30 21:32:37 +0800

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   root: true,
   extends: ['@sxzz/eslint-config-vue', '@sxzz/eslint-config-prettier'],
   rules: {
+    'vue/prefer-import-from-vue': 'off',
     'vue/no-ref-as-operand': 'off',
   },
 })
