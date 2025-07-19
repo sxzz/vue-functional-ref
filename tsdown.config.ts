ce3b8b4537c81c97434d03d692bba1735b910eca
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-07-20 05:50:11 +0800

    feat!: drop cjs build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/*.ts', '!./src/common.ts'],
-  format: ['cjs', 'esm'],
-  target: 'node18.12',
   external: ['@vue/reactivity', '@vue/shared'],
-  dts: true,
-  clean: true,
 })
