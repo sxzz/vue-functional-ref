eeeb8ed04c8f6415d2a7489dacae1f9faaefb7d7
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-24 09:45:59 +0800

    chore: cleanup lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
 import { sxzz } from '@sxzz/eslint-config'
-export default sxzz({
-  rules: {
-    'vue/no-ref-as-operand': 'off',
-    'vue/prefer-import-from-vue': 'off',
-  },
-})
+
+export default sxzz()
